b0dc70988b886d2bb5003df45e94c101a49ad0f3
Coby Yates <cobylyates@gmail.com>
2019-04-05 10:56:16 -0600

    First Draft with requirements met

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Custom JS here
+
+$('#myList a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
+
+$('.carousel').carousel({
+    keyboard: true,
+    touch: true
+})
